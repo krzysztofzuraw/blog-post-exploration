30066d457275dc2888b6d6814b2e4ccab9b124b3
Krzysztof Żuraw <services@kzuraw.com>
2020-05-05 18:15:09 +0200

    feat: add colors & fonts to styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as ReactDOM from 'react-dom';
 import React, { FunctionComponent } from 'react';
 
+import './index.scss';
+
 const Root: FunctionComponent = () => <div>Template</div>;
 
 ReactDOM.render(<Root />, document.getElementById('root') as HTMLElement);
